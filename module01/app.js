a20441a33a387d853543ea69164cfc66efe08b91
donald.tang <donald.tangze@outlook.com>
2018-11-19 21:15:25 +1100

    modify

--- module01/app.js
+++ module01/app.js
@@ -4,39 +4,30 @@
 
 angular.module('lunchcheck', [])
 .controller('MyFirstController', MyFirstController)
-.filter('mySplit', function() {
-  return function(input) {
-    var out = input.split(",");
-    return out;
-  };
-})
+.filter('mySplit', MySplit);
 //.filter('mySplit', MySplit);
 
 MyFirstController.$inject = ['$scope', 'mySplitFilter'];
 
 function MyFirstController($scope, mySplitFilter){
-  var string = "12,345,43";
- //不能通过=号传值 string = $scope.menu;
-
  $scope.checkNum = function(){
-   var num = ($scope.menu | mySplitFilter);
+   var num =  mySplitFilter($scope.menu);
    if(num.length < 1){
      $scope.message="Please enter data first";
    }else if (num.length <=3) {
      $scope.message="Enjoy!";
-   }else if (true) {
+   }else if (num.length > 3) {
      $scope.message="Too much!";
    }
  };
-
 }
 
-// function MySplit() {
-//   return function(input) {
-//     var out = input.split(",");
-//     return out;
-//   };
-// }
+function MySplit() {
+  return function(input) {
+    var out = input.split(",");
+    return out;
+  };
+}
 // App.filter("MySplit", function(){
 //     return function(text){
 //         return text.split(",");
