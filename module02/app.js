716deeb175ef7f1eba56fa65045051e4e84b3580
donald.tang <donald.tangze@outlook.com>
2018-11-23 18:26:49 +1100

    assignment of week 2

--- module02/app.js
+++ module02/app.js
@@ -1,21 +1,50 @@
 (function(){
 
+
 angular.module("ShoppingListCheckOff", [])
 .controller("ToBuyController", ToBuyController)
-.controller("AlreadyBoughtController", AlreadyBoughtController);
+.controller("AlreadyBoughtController", AlreadyBoughtController)
+.service("ShoppingListCheckOffService", ShoppingListCheckOffService);
 
-function ToBuyController($scope){
-  $scope.toBuyItemList=[{ name: "cookies", quantity: 10 },{ name: "cocacola", quantity: 4 }];
+ToBuyController.$inject = ['$scope', "ShoppingListCheckOffService"];
+function ToBuyController($scope,ShoppingListCheckOffService){
 
-  var buyItem = fucntion(){
-    console.log($scope);
+  this.toBuyItemList = ShoppingListCheckOffService.getToBuyItems();
+
+  this.buyItem = function (index) {
+    ShoppingListCheckOffService.removeItem(index);
+    ShoppingListCheckOffService.addItem(index);
   }
 }
 
+AlreadyBoughtController.$inject = ['$scope', 'ShoppingListCheckOffService'];
+function AlreadyBoughtController($scope, ShoppingListCheckOffService){
+  this.boughtItemList = ShoppingListCheckOffService.getBoughtItems();
+}
 
-function AlreadyBoughtController($scope){
-  $scope.boughtItemList=[{ name: "cookies", quantity: 10 },{ name: "cocacola", quantity: 4 }];
+function ShoppingListCheckOffService(){
+  var toBuyItemList=[{ name: "pancake", quantity: "10 pieces" },{ name: "cocacola", quantity: "4 bottles" },
+                    { name: "banana", quantity: "10 kiloes" },{ name: "avocado", quantity: "4 kiloes" },
+                    { name: "blueberry", quantity: "7 kiloes" },{ name: "apple", quantity: "6 kiloes" },
+                    { name: "raseberry", quantity: "8 kiloes" },{ name: "pear", quantity: "4 kiloes" }];
+  var boughtItemList=[];
 
+  this.removeItem = function (index){
+    toBuyItemList.splice(index,1);
+  }
+
+  this.addItem = function (index){
+    var item = toBuyItemList[index];
+    boughtItemList.push(item);
+  }
+
+  this.getToBuyItems = function (){
+    return toBuyItemList;
+  }
+
+  this.getBoughtItems = function (){
+    return boughtItemList;
+  }
 }
 
 })()
